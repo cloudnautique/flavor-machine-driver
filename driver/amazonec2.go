79b16aab4644df63f70e5e7d49ed908ad26270c5
Josh Curl <josh@curl.me>
2017-01-10 14:43:25 -0800

    Setup flags and handle Amazon initialization

---
+++ driver/amazonec2.go
@@ -0,0 +1,147 @@
+package rancher
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+func (d *Driver) setupAmazon() error {
+	client := d.AmazonEC2Driver.GetClient().(*ec2.EC2)
+
+	vpcId, err := findOrCreateVpc(client)
+	if err != nil {
+		return err
+	}
+	d.AmazonEC2Driver.VpcId = vpcId
+
+	subnetId, availabilityZone, err := findOrCreateSubnet(client, vpcId)
+	if err != nil {
+		return err
+	}
+	d.AmazonEC2Driver.SubnetId = subnetId
+	d.AmazonEC2Driver.Zone = string(availabilityZone[len(availabilityZone)-1])
+
+	if _, err = client.CreateTags(&ec2.CreateTagsInput{
+		Resources: []*string{
+			&[]string{vpcId}[0],
+			&[]string{subnetId}[0],
+		},
+		Tags: []*ec2.Tag{
+			&ec2.Tag{
+				Key:   &[]string{"Name"}[0],
+				Value: &[]string{tag}[0],
+			},
+		},
+	}); err != nil {
+		return err
+	}
+
+	securityGroupId, err := findOrCreateSecurityGroup(client, vpcId)
+	if err != nil {
+		return err
+	}
+	d.AmazonEC2Driver.SecurityGroupId = securityGroupId
+
+	return nil
+}
+
+func findOrCreateVpc(client *ec2.EC2) (string, error) {
+	describeVpcsOutput, err := client.DescribeVpcs(&ec2.DescribeVpcsInput{
+		Filters: []*ec2.Filter{
+			&ec2.Filter{
+				Name: &[]string{"tag:Name"}[0],
+				Values: []*string{
+					&[]string{tag}[0],
+				},
+			},
+		},
+	})
+	if err != nil {
+		return "", err
+	}
+
+	vpcs := describeVpcsOutput.Vpcs
+
+	if len(vpcs) == 0 {
+		createVpcOutput, err := client.CreateVpc(&ec2.CreateVpcInput{
+			CidrBlock: &[]string{vpcCidnBlock}[0],
+		})
+		if err != nil {
+			return "", err
+		}
+		return *createVpcOutput.Vpc.VpcId, nil
+	} else if len(vpcs) == 1 {
+		return *vpcs[0].VpcId, nil
+	}
+
+	return "", fmt.Errorf("Multiple VPCs named %s found", tag)
+}
+
+func findOrCreateSubnet(client *ec2.EC2, vpcId string) (string, string, error) {
+	describeSubnetsOutput, err := client.DescribeSubnets(&ec2.DescribeSubnetsInput{
+		Filters: []*ec2.Filter{
+			&ec2.Filter{
+				Name: &[]string{"tag:Name"}[0],
+				Values: []*string{
+					&[]string{tag}[0],
+				},
+			},
+		},
+	})
+	if err != nil {
+		return "", "", err
+	}
+
+	subnets := describeSubnetsOutput.Subnets
+
+	if len(subnets) == 0 {
+		createSubnetOutput, err := client.CreateSubnet(&ec2.CreateSubnetInput{
+			CidrBlock: &[]string{subnetCidnBlock}[0],
+			VpcId:     &[]string{vpcId}[0],
+		})
+		if err != nil {
+			return "", "", err
+		}
+		return *createSubnetOutput.Subnet.SubnetId, *createSubnetOutput.Subnet.AvailabilityZone, nil
+	} else if len(subnets) == 1 {
+		return *subnets[0].SubnetId, *subnets[0].AvailabilityZone, nil
+	}
+
+	return "", "", fmt.Errorf("Multiple subnets named %s found", tag)
+}
+
+func findOrCreateSecurityGroup(client *ec2.EC2, vpcId string) (string, error) {
+	describeSecurityGroupsOutput, err := client.DescribeSecurityGroups(&ec2.DescribeSecurityGroupsInput{
+		Filters: []*ec2.Filter{
+			&ec2.Filter{
+				Name: &[]string{"group-name"}[0],
+				Values: []*string{
+					&[]string{tag}[0],
+				},
+			},
+		},
+	})
+	if err != nil {
+		return "", err
+	}
+
+	securityGroups := describeSecurityGroupsOutput.SecurityGroups
+
+	if len(securityGroups) == 0 {
+		createSecurityGroupOutput, err := client.CreateSecurityGroup(&ec2.CreateSecurityGroupInput{
+			Description: &[]string{tag}[0],
+			GroupName:   &[]string{tag}[0],
+			VpcId:       &[]string{vpcId}[0],
+		})
+		if err != nil {
+			return "", err
+		}
+		fmt.Println(createSecurityGroupOutput)
+		return *createSecurityGroupOutput.GroupId, nil
+	} else if len(securityGroups) == 1 {
+		return *securityGroups[0].GroupId, nil
+	}
+
+	return "", fmt.Errorf("Multiple security groups named %s found", tag)
+}
--- driver/driver.go
+++ driver/driver.go
@@ -1,18 +1,52 @@
 package rancher
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/docker/machine/drivers/amazonec2"
 	"github.com/docker/machine/drivers/digitalocean"
 	"github.com/docker/machine/libmachine/drivers"
+	"github.com/docker/machine/libmachine/drivers/rpc"
 	"github.com/docker/machine/libmachine/mcnflag"
 	"github.com/docker/machine/libmachine/state"
 	"github.com/packethost/docker-machine-driver-packet"
 )
 
+const (
+	tag             = "Rancher Cloud"
+	vpcCidnBlock    = "10.0.0.0/16"
+	subnetCidnBlock = "10.0.0.0/24"
+)
+
+var (
+	apiKeyFlagNames = []string{
+		// Amazon
+		"access-key",
+		"secret-key",
+
+		// Digital Ocean
+		"access-token",
+
+		// Packet
+		"api-key",
+		"project-id",
+	}
+)
+
+type Flavor struct {
+	Provider      string
+	DriverOptions map[string]interface{}
+}
+
 var _ drivers.Driver = &Driver{}
 
 type Driver struct {
 	*drivers.BaseDriver
+
+	AvailableFlavors map[string]Flavor
+	flavor           Flavor
+
 	AmazonEC2Driver    *amazonec2.Driver
 	DigitalOceanDriver *digitalocean.Driver
 	PacketDriver       packet.Driver
@@ -28,7 +62,12 @@ func NewDriver(hostName, storePath string) *Driver {
 	}
 }
 
-func (d *Driver) setupInnerDriver() {
+func (d *Driver) DriverName() string {
+	return "rancher"
+}
+
+func (d *Driver) setupInnerDriver() error {
+	fmt.Println("setupInnerDriver")
 	if d.AmazonEC2Driver == nil {
 		d.AmazonEC2Driver = amazonec2.NewDriver(d.MachineName, d.StorePath)
 	}
@@ -40,25 +79,90 @@ func (d *Driver) setupInnerDriver() {
 	d.PacketDriver.MachineName = d.MachineName
 	d.PacketDriver.StorePath = d.StorePath
 
-	d.Driver = d.DigitalOceanDriver
+	if d.flavor.Provider == "amazonec2" {
+		d.Driver = d.AmazonEC2Driver
+	} else if d.flavor.Provider == "digitalocean" {
+		d.Driver = d.DigitalOceanDriver
+	} else if d.flavor.Provider == "packet" {
+		d.Driver = &d.PacketDriver
+	}
+
+	return nil
 }
 
 func (d *Driver) GetCreateFlags() []mcnflag.Flag {
-	d.setupInnerDriver()
-	var flags []mcnflag.Flag
-	flags = append(flags, d.AmazonEC2Driver.GetCreateFlags()...)
-	flags = append(flags, d.DigitalOceanDriver.GetCreateFlags()...)
-	flags = append(flags, d.PacketDriver.GetCreateFlags()...)
+	// Rancher specific flags
+	flags := []mcnflag.Flag{
+		mcnflag.StringFlag{
+			Name: "flavor",
+		},
+	}
+
+	// Borrow API key flags from inner drivers
+	var innerFlags []mcnflag.Flag
+	innerFlags = append(innerFlags, d.AmazonEC2Driver.GetCreateFlags()...)
+	innerFlags = append(innerFlags, d.DigitalOceanDriver.GetCreateFlags()...)
+	innerFlags = append(innerFlags, d.PacketDriver.GetCreateFlags()...)
+	for _, innerFlag := range innerFlags {
+		for _, name := range apiKeyFlagNames {
+			if strings.Contains(innerFlag.String(), name) {
+				flags = append(flags, innerFlag)
+			}
+		}
+	}
+
 	return flags
 }
 
-func (d *Driver) DriverName() string {
-	return "rancher"
+// Merge the three sources of flag values, from lowest priority to highest
+// Defaults from inner driver
+// Values determined by flavor
+// Values passed in via CLI (likely API keys)
+func getDriverOpts(mcnflags []mcnflag.Flag, flavorDriverOptions, cliDriverOptions map[string]interface{}) rpcdriver.RPCFlags {
+	driverOpts := rpcdriver.RPCFlags{
+		Values: make(map[string]interface{}),
+	}
+	for _, f := range mcnflags {
+		driverOpts.Values[f.String()] = f.Default()
+		if f.Default() == nil {
+			driverOpts.Values[f.String()] = false
+		}
+	}
+	for k, v := range flavorDriverOptions {
+		driverOpts.Values[k] = v
+	}
+	for k, v := range cliDriverOptions {
+		driverOpts.Values[k] = v
+	}
+	return driverOpts
 }
 
 func (d *Driver) SetConfigFromFlags(flags drivers.DriverOptions) error {
-	d.setupInnerDriver()
-	return d.Driver.SetConfigFromFlags(flags)
+	var ok bool
+	d.flavor, ok = d.AvailableFlavors[flags.String("flavor")]
+	if !ok {
+		return fmt.Errorf("Unrecognized flavor %s", flags.String("flavor"))
+	}
+
+	if err := d.setupInnerDriver(); err != nil {
+		return err
+	}
+
+	// TODO: try to avoid this type assertion
+	cliDriverOptions := flags.(*rpcdriver.RPCFlags)
+	driverOptions := getDriverOpts(d.Driver.GetCreateFlags(), cliDriverOptions.Values, d.flavor.DriverOptions)
+
+	if err := d.Driver.SetConfigFromFlags(driverOptions); err != nil {
+		return err
+	}
+
+	if d.flavor.Provider == "amazonec2" {
+		if err := d.setupAmazon(); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (d *Driver) GetSSHHostname() (string, error) {
